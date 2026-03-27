7ef804a4632070b29145d4039d22a3774d204ca6
JohnsonNguyen-coll <hoangnl1811@gmail.com>
2026-03-27 17:04:09 +0700

    fix: data access

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8072031aae07610e230ea9fed5de434fabc7df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28918c1bbfa9c47936f8ea26c8d480b186327b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
